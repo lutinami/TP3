ec7d8524fa4f8de7a840516b396877f6b871ddec
lutinami <102843725+lutinami@users.noreply.github.com>
2024-06-26 23:11:58 -0300

    Add files via upload

--- TP3.c
+++ TP3.c
@@ -40,18 +40,29 @@
 #include "MKL46Z4.h"
 #include "fsl_debug_console.h"
 /* TODO: insert other include files here. */
+
+/* BIBILIOTECAS EXTRA PARA GENERAR NUMEROS ALEATORIOS */
 #include<stdlib.h>
 #include<time.h>
+
 /* TODO: insert other definitions and declarations here. */
-// enums de ESTADOS
-typedef enum{NORMALIZADO, FUGA, VALVULA_CERRADA}ESTADOS;
-#define SEG30 30000000;
-#define RETARDO 150000;
+
+// Estados de la MEF
+typedef enum{OPEN_VALVE, GAS_WARNING, CLOSED_VALVE} ESTADOS;
+
+
+// Constantes de la implementacion
+//#define SEG30 30000000;		// 30 segundos aproximado
+#define SEG30 18750000
+#define RETARDO 150000;		// retardo para el parpadeo del led
+
+
 /*
  * @brief   Application entry point.
  */
 int main(void) {
 
+	// !!! COSAS DE LA PLACA !!! NO TOCAR !!!
 	/* Init board hardware. */
 	BOARD_InitBootPins();
 	BOARD_InitBootClocks();
@@ -60,6 +71,8 @@ int main(void) {
 	/* Init FSL debug console. */
 	BOARD_InitDebugConsole();
 #endif
+	// !!! COSAS DE LA PLACA !!! NO TOCAR !!!
+
 
 	// INICIALIZACION DE LOS COMPONENTES A UTILIZAR
 	// LED VERDE, PORT D, PIN 5
@@ -70,7 +83,7 @@ int main(void) {
 
 	// LED ROJO, PORT E, PIN 29
 	SIM->SCGC5 |= 1<<13;	// SETEA CLOCK
-	PORTE->PCR[29] |= 1<<8; 	// SETEA MUX
+	PORTE->PCR[29] |= 1<<8; // SETEA MUX
 	PTE->PCOR |= 1<<29;		// LIMPIA ORDENES
 	PTE->PDDR |= 1<<29;		// LO INCIALIZA COMO SALIDA
 
@@ -85,71 +98,86 @@ int main(void) {
 	PORTC->PCR[12] |= 3; 	// SETEA PULLS
 
 	// INICIALIZACION DE ESTADOS
-	ESTADOS estado_actual = NORMALIZADO;
-	ESTADOS proximo_estado = NORMALIZADO;
+	ESTADOS estado_actual = OPEN_VALVE;
+	ESTADOS proximo_estado = OPEN_VALVE;
 
 	// APAGO LOS LEDS
 	PTE->PTOR |= 1<<29;
 	PTD->PTOR |= 1<<5;
 
-	// TEMPORIZADOR ALEATORIO
-	uint32_t tiempo, treinta_segundos, tiempo_fuga;
-	treinta_segundos = SEG30;
+	// TIEMPOS A UTILIZAR
+	uint32_t tiempo, treinta_segundos, tiempo_fuga, titila;
+	titila = RETARDO;			// tiempo para el parpadeo
+	treinta_segundos = SEG30;	// 30 segundos
 	srand(time(NULL));
 
 	while(1) {
 		switch(estado_actual){
-		case NORMALIZADO:
-			if(!(PTC->PDIR & (1<<3))){		// si esta presionado el sw1
-				PTD->PTOR |= 1<<5;			// prendo el led verde
-				tiempo = 0;
-				proximo_estado = FUGA;
-			}
-			break;
-		case FUGA:
-			tiempo++;
-
-			if (tiempo > treinta_segundos){
-				PTE->PTOR |= 1<<29;
-				tiempo = RETARDO;
-				tiempo_fuga = 1000000*(rand() % 31);
-				proximo_estado = VALVULA_CERRADA;
-			}
-
-			if(PTC->PDIR & (1<<3)){			// si se suelta el sw1
-				PTD->PTOR |= 1<<5;			// apago el led verde
-				proximo_estado = NORMALIZADO;
-			}
-			break;
-		case VALVULA_CERRADA:
-			if(tiempo > 0){
-				tiempo--;
-			}
-
-			if(tiempo_fuga > 0){
-				tiempo_fuga--;
-			}
-
-			if(tiempo_fuga == 0){
-				PTD->PSOR |=1<<5;
-			}
-
-			if(tiempo == 0){
-				PTE->PTOR |= 1<<29;
-				tiempo = RETARDO;
-			}
-
-			if(!(PTC->PDIR & (1<<12))){
-				PTE->PSOR |= 1<<29;
-				proximo_estado = NORMALIZADO;
-			}
-
+		// VALVULA ABIERTA --------------------------------------------------
+		case OPEN_VALVE:						// Si el sistema funciona con
+			PTD->PCOR |= 1<<5;					// normalidad, el led verde
+			if(!(PTC->PDIR & (1<<3))){			// permancece prendido.
+				PTD->PTOR |= 1<<5;				// Si se detecta una concen-
+				PTE->PTOR |= 1<<29;				// tracion peligrosa de gas
+				tiempo = 0;						// en el ambiente (sw1 pres-
+				titila = RETARDO;				// ionado), se cambia de
+				proximo_estado = GAS_WARNING;	// estado.
+			}									//
+			break;								//
+		// ------------------------------------------------------------------
+		// CANTIDAD DE GAS EN EL AMBIENTE PELIGROSA ------------------------------
+		case GAS_WARNING:								// Mientras la concen-
+			tiempo++;									// tracion de gas en
+			if(titila > 0){								// el ambiente sea
+				titila--;								// peligrosa (sw1 pre-
+			}											// sionado), el led
+														// verde titila, y se
+			if(titila == 0){							// enciende el ex-
+				PTD->PTOR |= 1<<5;						// tractor (led rojo).
+				titila = RETARDO;						// Si esto persiste por
+			}											// mas de 30 segundos,
+			if (tiempo > treinta_segundos){				// se cierra la valvula
+				PTE->PSOR |= 1<<29;						// de gas, pasando a un
+				titila = RETARDO;						// nuevo estado. Sino,
+				tiempo_fuga = 625000*(rand() % 31);		// se vuelve a un estado
+				proximo_estado = CLOSED_VALVE;			// normalizado.
+			}											//
+														// Si se cierra la valvula,
+			if(PTC->PDIR & (1<<3)){						// se apaga el extractor, y
+				PTD->PCOR |= 1<<5;						// se determina un tiempo
+				PTE->PSOR |= 1<<29;						// para cual baje la con-
+				proximo_estado = OPEN_VALVE;			// centracion del gas.
+			}											//
+			break;										//
+		// ------------------------------------------------------------------------
+		// VALVULA CERRADA --------------------------------------------------------
+		case CLOSED_VALVE:							// El led verde titila hasta
+			if(titila > 0){							// que se llege al tiempo de
+				titila--;							// fuga determinado.
+			}										//
+													// Una vez que la concentracion
+			if(tiempo_fuga > 0){					// de gas en el ambiente se haya
+				tiempo_fuga--;						// normalizado, se debe abrir
+			}										// manualmente la valvula (pre-
+													// sionar sw3), cambiando de
+			if(tiempo_fuga == 0){					// estado a valvula abierta.
+				PTD->PSOR |=1<<5;					//
+				if(!(PTC->PDIR & (1<<12))){			//
+					proximo_estado = OPEN_VALVE;	//
+				}									//
+			}										//
+													//
+			if(titila == 0){						//
+				PTD->PTOR |= 1<<5;					//
+				titila = RETARDO;					//
+			}										//
+			break;									//
+			// -----------------------------------------------------------------
 		}
 		// si cambia el estado, cambio estado_actual
 		if(estado_actual != proximo_estado){
 			estado_actual = proximo_estado;
 		}
-
 	}
 	return 0 ;
 }
